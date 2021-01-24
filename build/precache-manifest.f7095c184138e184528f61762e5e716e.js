5335118035a11e76b7ed1d384bb722ca5c99585b
Shubham Salvi <shubham619.salvi619@gmail.com>
2021-01-25 05:06:01 +0530

    first commit

---
+++ build/precache-manifest.f7095c184138e184528f61762e5e716e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb0998ce02511fac5c55a015cb2987c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caec1dffb272742735d3",
+    "url": "/static/css/main.8adbba5a.chunk.css"
+  },
+  {
+    "revision": "b6742956d0d73730b709",
+    "url": "/static/js/2.ea59d149.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ea59d149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caec1dffb272742735d3",
+    "url": "/static/js/main.cbdb4b04.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
